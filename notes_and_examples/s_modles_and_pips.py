2682240785a94eca77b5ff6016a6397ab1961ce3
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-10-09 19:49:21 -0400

    adding python code

---
+++ notes_and_examples/s_modles_and_pips.py
@@ -0,0 +1,14 @@
+#modules and pip
+
+#module is a python file that we can import into our current python file 
+
+import useful_tools
+
+print(useful_tools.roll_dice(10))
+
+
+#you can go to the python module index and it has a lot of code written for you 
+#there are 2 types of modules, built in and external modules
+
+#how to import external python docs, go to python docs 
+
---
+++ notes_and_examples/useful_tools.py
@@ -0,0 +1,12 @@
+import random 
+
+feet_in_mile = 5280
+meters_in_kilometer = 1000
+beatles = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+
+#tells the extension of a filename...
+def get_file_ext(filename):
+    return filename[filename.index(".") + 1:]
+
+def roll_dice(num):
+    return random.randint(1, num)
